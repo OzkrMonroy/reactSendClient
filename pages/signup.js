c69331059e3580aa0f1659eeb8a88f53c642ba51
Oscar Monroy <oscarmonroy657@gmail.com>
2020-08-18 09:12:51 -0600

    Formik and you have been added. Formik initial setup is ready

--- pages/signup.js
+++ pages/signup.js
@@ -1,12 +1,89 @@
-import React from 'react'
-import Layout from '../components/Layout';
+import React from "react";
+import Layout from "../components/Layout";
+import { useFormik } from "formik";
 
 const Signup = () => {
+  const formik = useFormik({
+    initialValues: {
+      userName: '',
+      userEmail: '',
+      userPassword: ''
+    }
+  })
   return (
     <Layout>
-      <h1>signup o crear cuenta</h1>
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+        <h2 className="text-4xl font-sans font-bold text-gray-900 text-center my-4">
+          Crear cuenta
+        </h2>
+        <div className="flex justify-center mt-5">
+          <div className="w-full max-w-lg">
+            <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4" onSubmit={formik.handleSubmit}>
+              <div className="mb-4">
+                <label
+                  htmlFor="userName"
+                  className="block text-black text-sm font-bold mb-2"
+                >
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Nombre de usuario"
+                  id="userName"
+                  value={formik.values.userName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="userEmail"
+                  className="block text-black text-sm font-bold mb-2"
+                >
+                  Correo electrónico
+                </label>
+                <input
+                  type="email"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Correo electrónico"
+                  id="userEmail"
+                  value={formik.values.userEmail}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="userPassword"
+                  className="block text-black text-sm font-bold mb-2"
+                >
+                  Contraseña
+                </label>
+                <input
+                  type="password"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Contraseña"
+                  id="userPassword"
+                  value={formik.values.userPassword}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+              </div>
+
+              <input
+                type="submit"
+                className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold"
+                value="Crear cuenta"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
-}
- 
+};
+
 export default Signup;
